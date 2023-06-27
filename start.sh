2bfdda55b595cea8ae8f34086796ee20a119b49f
Ackor <akrxploit2@gmail.com>
2023-06-26 21:22:43 -0600

    update

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
 apt-get update && apt-get install -y libnss3
+apt install libnss
+apt install libnss3-dev libgdk-pixbuf2.0-dev libgtk-3-dev libxss-dev  if the above commands didn't work then go for the below one  sudo apt install libgconf-2-4 libatk1.0-0 libatk-bridge2.0-0 libgdk-pixbuf2.0-0 libgtk-3-0 libgbm-dev libnss3-dev libxss-dev
